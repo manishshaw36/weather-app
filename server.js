dfd58c1c4f91fd824813807a7ec29c6ade9121f2
Manish Shaw <manishshaw36@gmail.com>
2019-04-08 09:38:17 +0530

    second commit

---
+++ server.js
@@ -0,0 +1,39 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+var fahrenheitToCelsius = require('fahrenheit-to-celsius');
+
+const apiKey = '3b9adb99753510c1fe7ff68ba4f590da';
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body);
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${Math.round((fahrenheitToCelsius(Number(weather.main.temp)) * 100) / 100)} degree celcius in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
